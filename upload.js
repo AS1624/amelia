9b2422bd8056b2cef67deab24996ba745c4f9728
AS1624 <thebest1dudes@gmail.com>
2025-01-27 13:49:21 -0900

    fix placeholders

--- upload.js
+++
@@ -1,8 +0,0 @@
-function toBase64(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result.split(',')[1]); // Get the base64 string
-        reader.onerror = (error) => reject(error);
-        reader.readAsDataURL(file);
-    });
-}
