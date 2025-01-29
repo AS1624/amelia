6b27cb67d26374b961d5013d8880ca68d2ebb3dc
AS1624 <thebest1dudes@gmail.com>
2025-01-28 19:06:54 -0900

    50mb file limit

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 // Basic route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
