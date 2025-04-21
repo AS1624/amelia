20adec6e5a29bb6e5acbaef07bbf5c4e3fbdcd1b
AS1624 <thebest1dudes@gmail.com>
2025-04-21 14:52:07 -0800

    hopefully add all tags

--- enumerate.js
+++ enumerate.js
@@ -1,58 +1,62 @@
 async function getOutfits(search) {
     const url = `https://api.github.com/repos/as1624/amelia/contents/json`;
 
-    try {
-        const response = await fetch(url, {
-            headers: { "Accept": "application/vnd.github.v3+json" }
-        });
+    let output = [
+        [],
+        []
+    ]
+    const response = await fetch(url, {
+        headers: { "Accept": "application/vnd.github.v3+json" }
+    });
 
-        if (!response.ok) {
-            throw new Error(`GitHub API error: ${response.status} ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        console.log(data);
-        data.map(async file => {
-            try {
-                let json = await fetch("json/" + file.name).then(result => result.json())
-                let outfit = {
-                    name: json.name,
-                    description: json.description,
-                    tags: json.tags,
-                    blob: base64ToBlob(json.imageBase64),
-                }
-                let content = document.getElementById('content');
-
-                console.log(outfit);
-                let tags = outfit.tags.toString().toLowerCase().split(" ");
-                let innerContent = `<div id="outfit-container">
-                    <h3 id="outfit-name">${outfit.name}</h3>
-                    <div id="outfit-img-container">
-                    <a href="/outfit.html?id=${file.name}"></a>
-                        <img src="${URL.createObjectURL(outfit.blob)}" alt="">
-                    </div>
-                    <div id="outfit-description">${outfit.description}</div>
-                    <div id="tag-container">`
-                tags.forEach(tag => {
-                    innerContent += `<span id="outfit-tag">${tag}</span>`
-                })
-                innerContent += "</div></div>"
-                if(
-                    !search
-                    || tags.indexOf(search.toLowerCase()) > -1
-                    || outfit.description.indexOf(search.toLowerCase()) > -1
-                ) {
-                    content.innerHTML += innerContent
-                }
-            }
-            catch (error) {
-                console.log(error);
-            }
-        })
-
-    } catch (error) {
-        console.error("Error fetching repository contents:", error);
+    if (!response.ok) {
+        throw new Error(`GitHub API error: ${response.status} ${response.statusText}`);
     }
+
+    const data = await response.json();
+    console.log(data);
+    data.map(async file => {
+        let json = await fetch("json/" + file.name).then(result => result.json())
+        let outfit = {
+            name: json.name,
+            description: json.description,
+            tags: json.tags,
+            blob: base64ToBlob(json.imageBase64),
+        }
+        let content = document.getElementById('content');
+
+        console.log(outfit);
+        let tags = outfit.tags.toString().toLowerCase().split(" ");
+        let innerContent = `<div id="outfit-container">
+            <h3 id="outfit-name">${outfit.name}</h3>
+            <div id="outfit-img-container">
+            <a href="/outfit.html?id=${file.name}"></a>
+                <img src="${URL.createObjectURL(outfit.blob)}" alt="">
+            </div>
+            <div id="outfit-description">${outfit.description}</div>
+            <div id="tag-container">`
+        tags.forEach(tag => {
+            let index = tags.indexOf(tag);
+            if(index > -1){
+                output[1][index] ++
+            }
+            else {
+                output[0].push(tag)
+                output[1].push(1)
+            }
+            innerContent += `<span id="outfit-tag">${tag}</span>`
+        })
+        innerContent += "</div></div>"
+        if(
+            !search
+            || tags.indexOf(search.toLowerCase()) > -1
+            || outfit.description.indexOf(search.toLowerCase()) > -1
+        ) {
+            content.innerHTML += innerContent
+        }
+    })
+
+    return output
 }
 function base64ToBlob(base64, mimeType = '') {
     console.log(base64)
